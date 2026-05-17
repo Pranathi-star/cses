e30d69ebd6c4d454056a11518fef9cef802efda0
Pranathi <pranathik2001@gmail.com>
2026-05-17 12:30:58 +0530

    twoknights

--- IntroductoryProblems/7-TwoKnights.py
+++ IntroductoryProblems/7-TwoKnights.py
@@ -5,31 +5,18 @@ if __name__ == "__main__":
         if x < 0 or x >= k or y < 0 or y >= k:
             return False
         return True
-    
     cache = {}
     knightMoves = ((-2, +1), (-1, +2), (+1, +2), (+2, +1), (-2, -1), (-1, -2), (+1, -2), (+2, -1))
     k = int(input())
     for s in range(1, k + 1):
-        allPositions = ((s ** 2) * (s ** 2 - 1)) // 2
-        attackPositions = 0
-
-        if (s >= 3):
-            if s in cache:
-                print(cache[s])
-            else:
-                picked = set()
-                for i in range(s):
-                    for j in range(s):
-                        for x in knightMoves:
-                            new_x = i + x[0]
-                            new_y = j + x[1]
-                            if (i, j, new_x, new_y) not in picked and isValid(new_x, new_y, s):
-                                attackPositions += 1
-                                picked.add((i, j, new_x, new_y))
-                    
-                res = allPositions - (attackPositions // 2)
-                cache[s] = res
-                print(res)
+        if s in cache:
+            print(cache[s])
+        else:
+            allPositions = ((s ** 2) * (s ** 2 - 1)) // 2
+            attackPositions = 4 * (s - 1) * (s - 2)
+            res = allPositions - attackPositions
+            cache[s] = res
+            print(res)
 
         
 
