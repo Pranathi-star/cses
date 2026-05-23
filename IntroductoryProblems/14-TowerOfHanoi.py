706e1e1a2cc63cec9e73487ebcaab8ab5e541f22
Pranathi <pranathik2001@gmail.com>
2026-05-23 16:13:15 +0530

    toh

--- IntroductoryProblems/14-TowerOfHanoi.py
+++ IntroductoryProblems/14-TowerOfHanoi.py
@@ -1,17 +1,23 @@
 if __name__ == "__main__":
     n = int(input())
+
+    moves = []
     def towerOfHanoi(source, target, aux, num):
         if num == 1:
+            moves.append((source, target))
             return
         
         towerOfHanoi(source, aux, target, num - 1)
-        print(source, aux)
+
         towerOfHanoi(source, target, aux, 1)
-        print(source, target)
+
         towerOfHanoi(aux, target, source, num - 1)
-        print(aux, target)
+
 
     towerOfHanoi(1, 3, 2, n)
+    print(len(moves))
+    for i in moves:
+        print(i[0], i[1])
 
 
 
